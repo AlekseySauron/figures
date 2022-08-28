ee3e4c59f843bc7822d74f3e7615b7b896e44aaa
Shilov Aleksey <sau0kherson@gmail.com>
2022-08-28 22:59:59 +0300

    add constructors

--- pkg/delivery/httppkg/register.go
+++ pkg/delivery/httppkg/register.go
@@ -43,11 +43,14 @@ func (h *Handler) Posting(c *gin.Context) {
 		// figure = mathpkg.NewSquare(newTask)
 		figure = mathpkg.NewSquare(newTask.H, newTask.W)
 	} else if newTask.Figure == "circle" {
-		figure = mathpkg.Circle{R: newTask.H}
+		// figure = mathpkg.Circle{R: newTask.H}
+		figure = mathpkg.NewCircle(newTask.H)
 	} else if newTask.Figure == "rectangle" {
-		figure = mathpkg.Rectangle{H: newTask.H, W: newTask.W}
+		// figure = mathpkg.Rectangle{H: newTask.H, W: newTask.W}
+		figure = mathpkg.NewRectangle(newTask.H, newTask.W)
 	} else if newTask.Figure == "triangle" {
-		figure = mathpkg.Triangle{H: newTask.H, W: newTask.W}
+		// figure = mathpkg.Triangle{H: newTask.H, W: newTask.W}
+		figure = mathpkg.NewTriangle(newTask.H, newTask.W)
 	} else {
 		c.JSON(http.StatusBadRequest, "unknow Figure")
 		return
--- pkg/services/mathpkg/types.go
+++ pkg/services/mathpkg/types.go
@@ -23,3 +23,15 @@ type Geometry interface {
 func NewSquare(H float64, W float64) Geometry {
 	return &Square{H: H, W: W}
 }
+
+func NewCircle(H float64) Geometry {
+	return &Circle{R: H}
+}
+
+func NewRectangle(H float64, W float64) Geometry {
+	return &Rectangle{H: H, W: W}
+}
+
+func NewTriangle(H float64, W float64) Geometry {
+	return &Triangle{H: H, W: W}
+}
