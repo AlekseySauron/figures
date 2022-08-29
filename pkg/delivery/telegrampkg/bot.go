7b1d72a09198bf596c5eca1150af9c0f6fbbb79b
Shilov Aleksey <sau0kherson@gmail.com>
2022-08-30 01:30:16 +0300

    +chat id from upfate

--- pkg/delivery/telegrampkg/bot.go
+++ pkg/delivery/telegrampkg/bot.go
@@ -2,14 +2,14 @@ package telegrampkg
 
 import (
 	"fmt"
-	"strconv"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/spf13/viper"
 )
 
 type TelegramBot struct {
-	bot *tgbotapi.BotAPI
+	bot    *tgbotapi.BotAPI
+	ChatID int64
 }
 
 func NewBot() *TelegramBot {
@@ -20,14 +20,24 @@ func NewBot() *TelegramBot {
 		return nil
 	}
 
-	return &TelegramBot{bot}
+	var ChatID int64
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates := bot.GetUpdatesChan(u)
+	for update := range updates {
+		ChatID = update.Message.Chat.ID
+		break
+	}
+
+	return &TelegramBot{bot, ChatID}
 }
 
 func (tgbot *TelegramBot) Send(res string) {
 
-	chat_id_str := viper.GetString("telegram.chat_id")
-	chat_id_int, _ := strconv.Atoi(chat_id_str)
-	chat_id_int64 := int64(chat_id_int)
+	// chat_id_str := viper.GetString("telegram.chat_id")
+	// chat_id_int, _ := strconv.Atoi(chat_id_str)
+	// chat_id_int64 := int64(chat_id_int)
+	chat_id_int64 := tgbot.ChatID
 
 	// msg := tgbotapi.NewMessage(421964311, fmt.Sprint(res))
 	msg := tgbotapi.NewMessage(chat_id_int64, fmt.Sprint(res))
